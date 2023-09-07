647819efc54cd7e6bf556d6eaf56689a703e258a
ines lucas <lucasinesmaria@gmail.com>
2023-09-07 13:31:15 +0100

    can create a checklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "checklists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   # Show all checklists
-  get '/checklists', to: 'checklists#index'
+  # get '/checklists', to: 'checklists#index'
+
+  resources :checklists, only: [:index, :new, :create]
 end
